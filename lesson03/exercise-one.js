2dd434b1043f5d5d6bb0f6c6fe3af113b2e3fb2f
matthew <leematt5@uw.edu>
2021-03-07 09:31:20 -0800

    completed exercise

--- lesson03/exercise-one.js
+++ lesson03/exercise-one.js
@@ -1,1 +1,19 @@
 // JavaScript source code
+
+let items = ["test", "hello", "dance!", "sing", "jump"];
+
+console.log(items);
+
+items.push("last");
+
+console.log(items);
+
+items.splice(2, 1);
+
+console.log(items);
+
+let ret = "";
+
+ret += items.slice(0);
+
+console.log(ret);
