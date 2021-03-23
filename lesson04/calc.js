374bd9e51a30d8fe5c54130264d52dd2991e5edb
matthew <leematt5@uw.edu>
2021-03-22 19:59:40 -0700

    added message to result output

--- lesson04/calc.js
+++ lesson04/calc.js
@@ -5,9 +5,7 @@
 
 function square(x) {
     alert("Enter a number!");
-    let result = Number(prompt());
-    result = result * result;
-    alert(result);
-    return result;
+    let result = Number(prompt("Enter the number here."));
+    alert("The square of " + result + " is " + result*result);
 }
 
