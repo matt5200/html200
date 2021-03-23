46f1b402c30e38627da51c19443e0edd5119b273
matthew <leematt5@uw.edu>
2021-03-22 19:42:01 -0700

    created button calcualtor

---
+++ lesson04/calc.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+
+
+
+
+function square(x) {
+    alert("Enter a number!");
+    let result = Number(prompt());
+    result = result * result;
+    alert(result);
+    return result;
+}
+
