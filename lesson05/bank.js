299464bd553aab076abef01139859d82e403e3d8
matthew <leematt5@uw.edu>
2021-04-09 14:29:36 -0700

    added functions

--- lesson05/bank.js
+++ lesson05/bank.js
@@ -1,13 +1,26 @@
 function bankPrompt() {
+    console.log("Please enter one of the supported commands.");
     var response = prompt("Welcome to Bank of HTML5200!\n Press one of the following keys to take action on your account.\n Q to quit.\n W to make a withdrawl.\n D to deposit.\n B to view balance.");
     switch (response) {
         case "Q":
             return;
         case "W":
+            makeWithdrawl();
+            break;
         case "D":
+            makeDeposit();
+            break;
         case "B":
         default:
-            console.log("Please enter one of the supported commands.");
+            alert("Please enter one of the supported commands.");
     }
     bankPrompt();
+}
+
+function makeDeposit() {
+    var response = prompt("Please enter the amount you will deposit.", "$");
+}
+
+function makeWithdrawl() {
+    var response = prompt("Please enter the amount you will withdrawl.", "$");
 }
